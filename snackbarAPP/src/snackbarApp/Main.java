8d50add43d4548e68cf8c9169969ff14357279d2
Angelg10 <angelguzman97@gmail.com>
2020-06-01 16:00:00 -0400

    initial commit, filestructure

---
+++ snackbarAPP/src/snackbarApp/Main.java
@@ -0,0 +1,14 @@
+package snackbarApp;
+
+public class Main
+{
+	private static void workWithData()
+	{
+		System.out.println("snackbar is open");
+	}
+
+	public static void main(String[] args)
+	{
+		workWithData();
+	}
+}
